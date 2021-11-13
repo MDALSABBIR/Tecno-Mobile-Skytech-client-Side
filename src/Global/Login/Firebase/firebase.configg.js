fbe9761b1642427f7dd97c610ca0fdba79cdfd0d
MD.ALSABBIR <86665168+MDALSABBIR@users.noreply.github.com>
2021-11-13 20:02:04 +0600

    client side implement

---
+++ src/Global/Login/Firebase/firebase.configg.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDRNBYwDeiLNkkXFXCVYAMAlPUl6az4oDg",
+  authDomain: "tecno-mobile-skytech.firebaseapp.com",
+  projectId: "tecno-mobile-skytech",
+  storageBucket: "tecno-mobile-skytech.appspot.com",
+  messagingSenderId: "835122685590",
+  appId: "1:835122685590:web:37298371c4ea4ddf1f84b7"
+};
+  
+  export default firebaseConfig;
